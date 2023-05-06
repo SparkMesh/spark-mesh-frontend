0aa05b35e22ee69914e566abbd5ad1e50696e615
Mohammed Ahsan <earth.ahsan@gmail.com>
2023-05-06 16:36:20 +0600

    navbar navigation bottom animation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,32 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import { useRouter } from 'next/router'
 type Props = {}
+const CustomLink = ({ href, title, className ='' }: { href: string,title:string, className: string }) => {   
+  const router = useRouter()
+  
+  
+  return (
+    <Link href={href} className={`${className} relative group`}>{title}
+    <span className={`h-[1px] inline-block bg-black absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}></span>
+    </Link>)}
 
-function NavBar({}: Props) {
+const NavBar = ({}: Props) => {
+  
   return (
     <header className='w-full px-32 py-8 font-medium flex justify-between'>
       <nav>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/projects'>Projects</Link>
-        <Link href='/articles'>Articles</Link>
+        <CustomLink className='mr-4' href='/' title='Home'/>
+        
+        
+        <CustomLink className='mx-4' href='/about' title='About'></CustomLink>
+        <CustomLink className='mx-4' href='/projects' title='Projects'></CustomLink>
+        <CustomLink className='ml-4' href='/articles' title='Article<'></CustomLink>
 
       </nav>
       <Logo/>
+      
       <nav>
         <Link href='/' target={'_blank'}>T</Link>
         <Link href='/' target={'_blank'}>T</Link>
